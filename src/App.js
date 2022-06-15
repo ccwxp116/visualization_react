80c1441bd843ae8986b1ae6796ce210d2e736d23
ccwxp116 <cici.wsq@gmail.com>
2022-06-15 16:01:28 -0700

    kinda final

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import { useState } from 'react'
 import GraphS from './GraphS'
 import GraphM from './GraphM'
-import GraphT_L from './GraphT_L'
 
 function App() {
   const [resultState, set_resultState] = useState()
   const [resultState1, set_resultState1] = useState()
-  const [resultState2, set_resultState2] = useState()
 
   if (!resultState) {
     const link = 'http://10.1.40.71:2000/dagview/onnx_summary?model_name=yolov5s_lousun2_simplify&version=4.0.11'
@@ -30,20 +28,10 @@ function App() {
     })
   }
 
-  if (!resultState) {
-    const link = 'http://10.1.40.71:2000/dagview/onnx_summary?model_name=yolov5s_lousun2_simplify&version=4.0.11'
-    fetch(link)
-    .then(res => res.json())
-    .then((res) => {
-      set_resultState2(res)
-    })
-  }
-
   return (
     <div className="App">
       {resultState && <GraphS resultState={resultState}/>}
       {resultState1 && <GraphM resultState={resultState1}/>}
-      {resultState2 && <GraphT_L resultState={resultState1}/>}
     </div>
   );
 }
